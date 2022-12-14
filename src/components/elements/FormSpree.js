2005cd10d1aef5df88e8c009af9c724380588f4f
Macarie Razvan-Cristian <cristialex99@gmail.com>
2022-12-14 21:12:58 +0200

    GA
    ga

--- src/components/elements/FormSpree.js
+++ src/components/elements/FormSpree.js
@@ -3,9 +3,13 @@
 import React from 'react';
 import { useForm } from '@formspree/react';
 import Input from './Input';
+import useGaEventsTracker from './useGaEventsTracker';
 function ContactForm() {
   const [state, handleSubmit] = useForm("xgeqgwll");
+  const GAEventsTracker = useGaEventsTracker("External Links");
+
   if (state.succeeded) {
+      GAEventsTracker("clicked");
       return <h4 className="mt-0 mb-8">
        Thanks for joining.
         </h4>;
@@ -13,8 +17,8 @@ function ContactForm() {
   
   return (
       <form onSubmit={handleSubmit}>
-        <Input id="email" type="email" name='email' label="Subscribe" labelHidden hasIcon="right" placeholder="Your email address">
-          <button type='submit' className="button button-primary button-sm mr-0">
+        <Input  onClick={e => GAEventsTracker("clicked")} id="email" type="email" name='email' label="Subscribe" labelHidden hasIcon="right" placeholder="Your email address">
+          <button type='submit' className="button button-primary button-sm mr-0" onClick={e => GAEventsTracker("clicked")}>
             Join!
           </button>
         </Input>
